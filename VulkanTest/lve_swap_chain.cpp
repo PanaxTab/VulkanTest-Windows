1ca0fa328994232db9172681fcdd1baef0e062f6
Panagiotis Ioulianou <panagiotis.ioulianou36@gmail.com>
2024-11-01 10:45:24 +0200

    IT'S 3D, also fast mat4

--- VulkanTest/lve_swap_chain.cpp
+++ VulkanTest/lve_swap_chain.cpp
@@ -386,20 +386,21 @@ namespace lve {
 
     VkPresentModeKHR LveSwapChain::chooseSwapPresentMode(
         const std::vector<VkPresentModeKHR>& availablePresentModes) {
-        for (const auto& availablePresentMode : availablePresentModes) {
-            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
-                std::cout << "Present mode: Mailbox" << std::endl;
-                return availablePresentMode;
-            }
-        }
-
-        // for (const auto &availablePresentMode : availablePresentModes) {
-        //   if (availablePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR) {
-        //     std::cout << "Present mode: Immediate" << std::endl;
-        //     return availablePresentMode;
-        //   }
-        // }
-
+    //MAILBOX
+    //    for (const auto& availablePresentMode : availablePresentModes) {
+    //        if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+    //            std::cout << "Present mode: Mailbox" << std::endl;
+    //            return availablePresentMode;
+    //        }
+    //    }
+    //IMMEDIATE
+    //for (const auto &availablePresentMode : availablePresentModes) {
+    //  if (availablePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR) {
+    //    std::cout << "Present mode: Immediate" << std::endl;
+    //    return availablePresentMode;
+    //  }
+    //}
+    //FIFO
         std::cout << "Present mode: V-Sync" << std::endl;
         return VK_PRESENT_MODE_FIFO_KHR;
     }
--- VulkanTest/shaders/simple_shader.vert
+++ VulkanTest/shaders/simple_shader.vert
@@ -1,16 +1,18 @@
 #version 450
 
-layout(location=0) in vec2 position;
+layout(location=0) in vec3 position;
 layout(location=1) in vec3 color;
 
+layout(location = 0) out vec3 fragColor;
+
 layout(push_constant) uniform Push{
-	mat2 transform;
-	vec2 offset; //Order must match
+	mat4 transform;
 	vec3 color;	//the order of struct
 } push;
 
 void main() {
 	//4d vector
 	//{-1,-1} = top left, {0,0} = center
-	gl_Position = vec4(push.transform * position + push.offset,0.0,1.0);
+	gl_Position = push.transform * vec4(position,1.0);
+	fragColor = color;
 }
