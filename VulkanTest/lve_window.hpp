c13df2c30ffcd4015a6241295a03b12dd2795271
Panagiotis Ioulianou <panagiotis.ioulianou36@gmail.com>
2024-11-02 10:06:35 +0200

    controllable camera

--- VulkanTest/lve_window.hpp
+++ VulkanTest/lve_window.hpp
@@ -17,6 +17,8 @@ namespace lve {
 			VkExtent2D getExtent() { return{ static_cast<uint32_t>(width),static_cast<uint32_t>(height) }; }
 			bool wasWindowResized() { return frameBufferResized; }
 			void resetWindowResizedFlag() { frameBufferResized = false; }
+
+			GLFWwindow* getGLFWwindow() const { return window; };
 			void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
 		private:
 			static void frameBufferResizedCallback(GLFWwindow* window, int width, int height);
